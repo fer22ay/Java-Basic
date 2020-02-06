deeb9f33a714a89485489f1ed72424a7496f96ca
Fernando Ambrosio <fer22ay@gmail.com>
2020-02-05 22:06:30 -0600

    Realizando pruebas de los metodos de la Clase Estudiante

--- src/com/fernando/POO/Estudiante.java
+++ src/com/fernando/POO/Estudiante.java
@@ -13,7 +13,10 @@ public class Estudiante extends Persona {
     private String codigoEstudiante;
     private float calificacion;
 
-    public Estudiante() {
+    public Estudiante(String nombre, String apellido, int edad, String codigoEstudiante, float calificacion) {
+        super(nombre, apellido, edad);
+        this.codigoEstudiante = codigoEstudiante;
+        this.calificacion = calificacion;
     }
 
     public String getCodigoEstudiante() {
@@ -33,14 +36,14 @@ public class Estudiante extends Persona {
     }
 
     public void muestraNombre() {
-
+        System.out.println("Nombre : " + this.getNombre());
     }
 
     public void muestraApellido() {
-
+        System.out.println("Apellido : " + this.getApellido());
     }
 
     public void muestraEdad() {
-
+        System.out.println("Edad : " + this.getEdad());
     }
 }
--- src/com/fernando/clases/Main.java
+++ src/com/fernando/clases/Main.java
@@ -2,6 +2,7 @@ package com.fernando.clases;
 
 import com.fernando.POO.Alumnos;
 import com.fernando.POO.Auto;
+import com.fernando.POO.Estudiante;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
@@ -30,26 +31,31 @@ public class Main {
 //
 //        auto.meterLlave("1234567");
 //        auto.mando("enciende");
+//
+//        int cantAlumnos = Integer.parseInt(JOptionPane.showInputDialog("Cuantos alumnos tienes"));
+//        Alumnos a[] = new Alumnos[cantAlumnos];
+//        for (int i = 0; i < cantAlumnos; i++) {
+//            a[i] = new Alumnos(JOptionPane.showInputDialog("Ingrese el nombre del alumno: "), 
+//                    JOptionPane.showInputDialog("Ingresa el aula del alumno").charAt(0));
+//            a[i].setCalificacion(Float.parseFloat(JOptionPane.showInputDialog("Ingresa la calificacion del alumno : ")));
+//        }
+//        float suma = 0;
+//        float promedio = 0;
+//        int cantidadAlumnosPorAula = 0;
+//        char aula = JOptionPane.showInputDialog("Dime de que aula quieres ver y obtener su promedio").charAt(0);
+//        for (int i = 0; i < cantAlumnos; i++) {
+//            if (a[i].getAula() == aula) {
+//                suma = suma+a[i].getCalificacion();
+//                cantidadAlumnosPorAula++;
+//            }
+//        }
+//        promedio = suma/cantidadAlumnosPorAula;
+//        JOptionPane.showMessageDialog(null, promedio);
 
-        int cantAlumnos = Integer.parseInt(JOptionPane.showInputDialog("Cuantos alumnos tienes"));
-        Alumnos a[] = new Alumnos[cantAlumnos];
-        for (int i = 0; i < cantAlumnos; i++) {
-            a[i] = new Alumnos(JOptionPane.showInputDialog("Ingrese el nombre del alumno: "), 
-                    JOptionPane.showInputDialog("Ingresa el aula del alumno").charAt(0));
-            a[i].setCalificacion(Float.parseFloat(JOptionPane.showInputDialog("Ingresa la calificacion del alumno : ")));
-        }
-        float suma = 0;
-        float promedio = 0;
-        int cantidadAlumnosPorAula = 0;
-        char aula = JOptionPane.showInputDialog("Dime de que aula quieres ver y obtener su promedio").charAt(0);
-        for (int i = 0; i < cantAlumnos; i++) {
-            if (a[i].getAula() == aula) {
-                suma = suma+a[i].getCalificacion();
-                cantidadAlumnosPorAula++;
-            }
-        }
-        promedio = suma/cantidadAlumnosPorAula;
-        JOptionPane.showMessageDialog(null, promedio);
+        Estudiante es = new Estudiante("Maria", "Perez", 20, "5390", 10);
+        es.muestraNombre();
+        es.muestraApellido();
+        es.muestraEdad();
     }
 
 }
