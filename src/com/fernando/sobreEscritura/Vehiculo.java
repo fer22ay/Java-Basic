ecf3ac3d1db05ed97834f552feff291ef2ca760f
Fernando Ambrosio <fer22ay@gmail.com>
2020-02-06 19:49:17 -0600

    Agregando la clase Vehiculo

---
+++ src/com/fernando/sobreEscritura/Vehiculo.java
@@ -0,0 +1,42 @@
+package com.fernando.sobreEscritura;
+
+/**
+ *
+ * @author Fernando Ambrosio
+ * @version v0.1.0
+ * @since 2020
+ */
+public class Vehiculo {
+    
+    private String matricula;
+    private String marca;
+    private String modelo;
+
+    public Vehiculo() {
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+}
